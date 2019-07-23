812e07bd6559f38a9fa744d06a0d1d55e947ad93
mistercle <mistercle@gmail.com>
2019-07-23 17:10:49 +0900

    add title

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import CityList from './CityList'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import WeatherDetailScreen from './WeatherDetailScreen'
 
-const AppNavigator = createStackNavigator({
-  Home : {
-    screen : CityList
-  }
-})
+const AppNavigator = createStackNavigator(
+    {
+      CityList : CityList,
+      Detail : WeatherDetailScreen,
+    },
+    {
+      initialRouteName: 'CityList',
+    }
+  
+);
 
 export default createAppContainer(AppNavigator);
 
