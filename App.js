47e0c40f6e67ddd884890a4750cbd1f736b9e93f
mistercle <mistercle@gmail.com>
2019-07-23 14:49:48 +0900

    Hello world!

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style = {styles.textBig}>Hello world!</Text>
     </View>
   );
 }
@@ -16,4 +16,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textBig:{
+    fontSize : 32,
+  }
 });
